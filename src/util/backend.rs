b420060e87bc9684b5d6e34d04bc8d75203f9750
Mart van Buren <mart@fractic.io>
2026-04-30 07:55:40 +0900

    Update AWS behaviour version to v2026_01_12.

--- src/util/backend.rs
+++ src/util/backend.rs
@@ -88,7 +88,7 @@ register_ctx_singleton!(dyn LogsCtxView, dyn LogsBackend, |ctx: Arc<
     dyn LogsCtxView,
 >| async move {
     let region = Region::new(ctx.log_region().clone());
-    let shared_config = aws_config::defaults(BehaviorVersion::v2025_08_07())
+    let shared_config = aws_config::defaults(BehaviorVersion::v2026_01_12())
         .region(region)
         .load()
         .await;
